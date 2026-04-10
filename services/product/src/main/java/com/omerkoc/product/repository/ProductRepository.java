529b4524005b688472ace72750bf83a7d0d7922d
OmerKocx <36omerkoc3634@gmail.com>
2026-04-10 14:10:55 +0300

    commit

--- services/product/src/main/java/com/omerkoc/product/repository/ProductRepository.java
+++ services/product/src/main/java/com/omerkoc/product/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.omerkoc.product.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.omerkoc.product.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+    List<Product> findAllByIdInOrderById(List<Integer> ids);
 }
